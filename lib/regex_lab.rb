43b29c2eee6a20eb3bea73fbc86324a19c2fea9d
McKayla Lankau <lankaukk@yahoo.com>
2020-12-07 01:07:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(word)
+  !!word.match(/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  boolean = text.scan(/\bun\w+ing\b/)
+  #scan will return an array
 end
 
 def words_five_letters_long(text)
-
+  boolean = text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !!text.match(//)
 end
 
 def valid_phone_number?(phone)
-
+  !!phone.match(//)
 end
